9f387bebc0d4ba4d45c5f360c0fed739cbe6a63f
Anton <noten.b@hotmail.com>
2016-04-04 14:28:19 +0200

    movement basic

---
+++ Pong3DGame/Assets/Movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public Transform body;
+    public float speed;
+    private float x;
+    private float y;
+    private float z;
+
+	// Use this for initialization
+	void Start () {
+        x = body.transform.position.x;
+        y = body.transform.position.y;
+        z = body.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        x += moveX * Time.deltaTime * speed;
+        y += moveY * Time.deltaTime * speed;
+
+        body.transform.position = new Vector3(x, y, z);
+	}
+}
