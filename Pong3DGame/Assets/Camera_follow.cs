97ff599c3dfb47c880158cd64c98ddda2a4ecfe1
Anton <noten.b@hotmail.com>
2016-04-11 15:00:37 +0200

    camera

---
+++ Pong3DGame/Assets/Camera_follow.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_follow : MonoBehaviour {
+
+    public Transform body;
+    public Transform ball;
+    public Transform paddle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = body.position.x;
+        float y = body.position.y;
+        float z = body.position.z;
+
+        float targetX;
+        float targetY;
+        targetX = Mathf.Lerp(ball.position.x  , 0, 0.5F);
+        targetX = Mathf.Lerp(paddle.position.x, targetX, 0.33F);
+
+        targetY = Mathf.Lerp(ball.position.y, 0, 0.5F);
+        targetY = Mathf.Lerp(paddle.position.y, targetY, 0.33F);
+
+        x = Mathf.Lerp(targetX, x, 1 - Time.deltaTime); //not framerate independant but close enough
+        y = Mathf.Lerp(targetY, y, 1 - Time.deltaTime);
+
+        body.position = new Vector3(x, y, z);
+        
+        body.LookAt(new Vector3(targetX, targetY, ball.position.z));
+	}
+}
