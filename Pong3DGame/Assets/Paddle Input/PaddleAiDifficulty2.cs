c980a4f695cf632add90dc318f2c74a8dd237cf9
Fractive <Fractive@live.com>
2016-05-02 15:44:11 +0200

    AI

---
+++ Pong3DGame/Assets/Paddle Input/PaddleAiDifficulty2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleAiDifficulty2 : PaddleInputParent {
+
+    public BallBehavior ballVars;
+    public Movement movementVars;
+
+	// Use this for initialization
+    public override Vector2 GetInput()
+    {
+        float x = Mathf.Sign(ballVars.x - movementVars.body.position.x) * 0.6F;
+        float y = Mathf.Sign(ballVars.y - movementVars.body.position.y) * 0.6F;
+
+
+        return new Vector2(x,y);
+    }
+}
